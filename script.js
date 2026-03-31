95a1629b35c6fc3e764689158c5dd06ce7363b6a
Kellyan01 <depriester.yoann@neuf.fr>
2026-03-31 20:16:23 +0200

    Lesson : Priority Queue

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ container.addEventListener("click", (event)=>{
 
 //Lancement de l'algorithme A*
 pathfinderBtn.addEventListener("click", ()=>{
-    const path = aStar(mainGrid, start, end, false, false);
+    const path = aStar(mainGrid, start, end, true, false, distanceOctile);
 
     if(!path){
         alert("Aucun chemin trouvé !");
