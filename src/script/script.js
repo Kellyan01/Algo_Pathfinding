4f5586d138ac46f85d542ae1eb513fac1e820b53
Kellyan01 <depriester.yoann@neuf.fr>
2026-04-02 11:29:37 +0200

    FIX : fixe obligatoirement la valeur de minSize et maxSize comme étant supérieur à 2 pour éviter des salles collées au bords et sans murs dans le Dungeon Generator

--- src/script/script.js
+++ src/script/script.js
@@ -180,9 +180,9 @@ generateDungeonBSPBtn.addEventListener("click", ()=>{
     const minSize = parseInt(document.getElementById("roomMinSize").value);
     const maxSize = parseInt(document.getElementById("roomMaxSize").value);
 
-    //Vérifier si minSize et maxSize sont supérieur à zéro
-    if(!minSize > 0 || !maxSize > 0){
-        alert("Les tailles doivent toutes être supérieur à 0");
+    //Vérifier si minSize et maxSize sont supérieur à 2
+    if(!minSize > 2 || !maxSize > 2){
+        alert("Les tailles doivent toutes être supérieur à 2");
         return;
     }
 
